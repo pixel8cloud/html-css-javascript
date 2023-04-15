80e2aab0c20d4ee73eafdafb27fb7dba8496045a
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-04-15 07:33:31 +0530

    video 31

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-console.log("hello world"+" pixel8cloud")
+// a = 10;
+// num1 = 10;
+
+// console.log("The value of num1++ is " + num1++);
+// console.log("The value of num1 " + num1);
+// console.log("The value of ++num1 is " + ++num1);
+// console.log("The value of num1 " + num1);
+// console.log("The value of num1-- is " + num1--);
+// console.log("The value of num1 " + num1);
+// console.log("The value of --num1 is " + --num1);
+// console.log("The value of num1 " + num1);
+
+// if (a < 11) {
+//   console.log("true value");
+// } else if (a == 12) {
+//   console.log("false value");
+// } else {
+//   console.log("no value");
+// }
+
+// switch (a) {
+//   case 10:
+//     console.log("true value");
+//     break;
+
+//   case 12:
+//     console.log(" value is 12 ");
+//     break;
+
+//   default:
+//     console.log(" default");
+//     break;
+// }
+
+// a = 12;
+// s = "Hello World";
+// p = " by pixel8cloud";
+// console.log(s + p);
+// s = s + p;
+// console.log(s);
+
+// console.log(`${a} value`);
+
+// console.log(s[0]);
+
+// function click() {
+//   console.log(`I'm "Clicked"`);
+// }
+
+setTimeout(() => {
+    console.log(`Set Timeout`);
+  }, 5000);
+  
+  setInterval(() => {
+    console.log(`Set Interval`);
+  }, 5000);
+  
+  // click();
+  
