81c950e4cbc0ca60fbc85ebb5a5139cbe27885dc
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-04-19 08:02:17 +0530

    video 33

--- main.js
+++ main.js
@@ -79,3 +79,68 @@
 
 // change style
 // input.style.display="none"
+
+// Date Time
+// let dateNow = new Date();
+// let dateNow = new Date(1000);
+// let dateNow = new Date("2023-04-19");
+
+// let dateNow = new Date(year, month, date, hours, minutes, seconds, milliseconds);
+// let dateNow = new Date(2023, 1, 19, 4, 30, 59, 23);
+// console.log(dateNow);
+// console.log(dateNow.getFullYear());
+// dateNow.setFullYear(2022);
+// console.log(dateNow);
+
+// Arrow FUnction
+// let sum = (a, b) => a + b;
+// let sum2 = (a, b) => {
+//   c = a + b;
+//   return c / 2;
+// };
+
+// let obj = {
+//   names: [1, 2, 3, 4, 5],
+//   greet: "Hello",
+//   speak() {
+//     this.names.forEach((i) => {
+//       console.log(i + " " + this.greet);
+//     });
+//   },
+// };
+
+// obj.speak();
+
+
+// MATH
+// let m = Math;
+// console.log(m.PI);
+// a = 10;
+// b = 20;
+
+// console.log(m.ceil(a));
+// console.log(m.floor(a));
+// console.log(m.ceil(b));
+// console.log(m.floor(b));
+
+// let c = a + (b - a) * m.random();
+// console.log(c);
+
+
+// JSON
+// var dict = {
+//   name: "pixel8cloud",
+//   age: "1 years",
+//   "upload video": "on yt",
+// };
+// dict["name"] = "pixel";
+
+// console.log(dict);
+// let jsonString = JSON.stringify(dict);
+// console.log(jsonString);
+
+// jsonString = jsonString.replace("pixel", "pixel8cloud");
+
+// console.log(jsonString);
+// let jsonObject = JSON.parse(jsonString);
+// console.log(jsonObject);
